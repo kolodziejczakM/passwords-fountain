8fd72c6f745de4b99952a0086df87486ebaa29de
kolodziejczakM <kolodziejczak.mn@gmail.com>
2020-01-31 19:55:09 +0100

    Context types improved

--- src/modules/localisation/localisation.context.tsx
+++ src/modules/localisation/localisation.context.tsx
@@ -3,10 +3,19 @@ import { createContext } from 'preact';
 import { useContext, useState, useMemo } from 'preact/hooks';
 import i18n from '../../i18n';
 
-const LocalisationContext = createContext(i18n.language);
+type ContextValue = [string, (v: string) => void];
 
-// TODO: make these types better
-export const LocalisationProvider = (props: any): any => {
+const LocalisationContext = createContext<ContextValue>([
+    i18n.language,
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    (): void => {},
+]);
+
+export const LocalisationProvider = ({
+    children,
+}: {
+    children: VNode;
+}): VNode => {
     const [currentLanguage, setLanguage] = useState(i18n.language);
 
     const setCurrentLanguage = (lang: string): void => {
@@ -14,16 +23,20 @@ export const LocalisationProvider = (props: any): any => {
         setLanguage(lang);
     };
 
-    const value = useMemo(() => [currentLanguage, setCurrentLanguage], [
-        currentLanguage,
-    ]);
+    const value: ContextValue = useMemo(
+        () => [currentLanguage, setCurrentLanguage],
+        [currentLanguage]
+    );
 
-    return <LocalisationContext.Provider value={value} {...props} />;
+    return (
+        <LocalisationContext.Provider value={value}>
+            {children}
+        </LocalisationContext.Provider>
+    );
 };
 
-// TODO: make these types better
-export const useLocalisation = (): any => {
-    const context = useContext(LocalisationContext);
+export const useLocalisation = (): ContextValue => {
+    const context: ContextValue = useContext(LocalisationContext);
 
     if (!context) {
         throw new Error(
@@ -34,12 +47,7 @@ export const useLocalisation = (): any => {
     return context;
 };
 
-// TODO: looks like there is an error in Preact VNode type,
-// It should be allowed to just return strings as jsx elements (without wrapping them in Fragment and making them VNodes)
 export const Text = ({ children }: { children: string }): VNode<string> => {
     useContext(LocalisationContext);
-    console.log('Re-rendering <Text /> Component');
-
-    // return i18n._(children);
     return <Fragment>{i18n._(children)}</Fragment>;
 };
