3880b03b1ebec5466c40334b1169d7e61bab115b
kolodziejczakM <kolodziejczak.mn@gmail.com>
2020-04-03 18:32:35 +0200

    feat(pages): unified pages definition

--- src/routes/passwordList/passwordList.tsx
+++ src/routes/passwordList/passwordList.tsx
@@ -2,5 +2,5 @@ import { h } from 'preact';
 import { TypedComponent } from '@/common/typings/prop-types';
 
 export const PasswordList: TypedComponent<{}> = () => (
-    <span>Password list page</span>
+    <section>Password list page</section>
 );
--- src/routes/settings/settings.tsx
+++ src/routes/settings/settings.tsx
@@ -1,4 +1,6 @@
 import { h } from 'preact';
 import { TypedComponent } from '@/common/typings/prop-types';
 
-export const Settings: TypedComponent<{}> = () => <span>Settings page</span>;
+export const Settings: TypedComponent<{}> = () => (
+    <section>Settings page</section>
+);
